4500978e7849688fb465f89197a7924aecdbf939
fredrikljung93 <frljung@kth.se>
2014-12-16 16:55:23 +0100

    App is now restartable

--- src/se/kth/shaketest/MainActivity.java
+++ src/se/kth/shaketest/MainActivity.java
@@ -15,6 +15,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -65,6 +67,26 @@ public class MainActivity extends Activity {
 	public void onResume() {
 		super.onResume();
 		startListening();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.restart) {
+			fallHasCome = false;
+			shakeCounter=0;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	private void startListening() {
